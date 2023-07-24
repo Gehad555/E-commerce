6368e0c4cb01397c38a22e7b0bb5f27e8ad9eedb
Gehad Shalaby <119525675+Gehad555@users.noreply.github.com>
2023-07-24 21:33:25 +0300

    Initial commit

---
+++ backend/middleware/validation.js
@@ -0,0 +1,9 @@
+const joi = require('joi');
+
+/*
+function validation (res,req,next){
+    firstname = joi.string().min(3).max(30).required();
+    lastname = joi.string().min(3).max(30).required();
+    email = joi.string().emain().required();
+}
+*/
