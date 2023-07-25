d5157370454e68e8110e00a806aed4dcd8a5f5fd
YasmeenFakhri <120497835+YasmeenFakhri@users.noreply.github.com>
2023-07-25 10:02:03 +0200

    add files
    add some files

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,19 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
+require('./config/db')
+const userRouter = require('./routes/UserRouter') 
+
+const port = 8000
+
+
+
+app.use(express.json())
+
+
+
+
+
+app.listen(port , ()=>{
+    console.log(`Server Runing on http://localhost${port}`)
+})
